1808e70252e7d550243085b57bb14b170282642c
ggiuffre <giorgiogiuffre@tin.it>
2017-05-31 08:57:36 +0200

    Add tests for back end

--- DBNsite/DBNlogic/util.py
+++ DBNsite/DBNlogic/util.py
@@ -25,14 +25,14 @@ class Configuration:
 
 
 def sigmoid(v):
-    """Return the element-wise sigmoid of a vector or matrix."""
+    """Return the element-wise sigmoid of a Numpy array."""
     ones = np.ones(v.shape) # (matrix of ones)
     return ones / (ones + np.exp(-v))
 
 def activation(v):
-    """Return the element-wise binary activation of a vector or matrix."""
+    """Return the element-wise binary activation of a Numpy array."""
     return v > np.random.uniform(size = v.shape)
 
 def squared_error(v, w):
-    """Return the mean squared error between two vectors or matrices."""
+    """Return the mean squared error between two Numpy arrays."""
     return ((v - w) ** 2).mean() # TODO: add sqrt?
---
+++ DBNsite/testing_DBNlogic/util_test.py
@@ -0,0 +1,40 @@
+import pytest
+import numpy as np
+import math
+
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(myPath, '..'))
+
+from DBNlogic.util import sigmoid, activation, squared_error
+
+def test_sigmoidDefinition():
+    """The `sigmoid` function applied to `x` returns 1 / (1 + e^-x)."""
+    for i in range(-1000,1001):
+        x = 0.1 * i
+        assert sigmoid(np.array(x)) == 1 / (1 + math.exp(-x))
+
+def test_sigmoidShape():
+    """The `sigmoid` function returns a Numpy array with
+    the same shape as the input array."""
+    v = np.random.randn(5, 12, 38)
+    assert sigmoid(v).shape == v.shape
+
+def test_sigmoidZero():
+    """The `sigmoid` function applied to 0 returns 0.5."""
+    x = 0
+    assert sigmoid(np.array(x)) == 0.5
+
+def test_sigmoidAsymptotes():
+    """The `sigmoid` function always stays between 0 and 1."""
+    for i in range(-100,101):
+        s = sigmoid(np.array(0.23 * i))
+        assert s > 0
+        assert s < 1
+
+def test_activationShape():
+    """The `activation` function returns a Numpy array with
+    the same shape as the input array."""
+    v = np.random.randn(5, 12)
+    assert activation(v).shape == v.shape
+
