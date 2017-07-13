4d679393d978a58889ab53b2b4d6225b3c3e4f0f
ggiuffre <giorgiogiuffre@tin.it>
2017-07-13 22:32:00 +0200

    Trying gunicorn...

--- DBNsite/DBNsite/wsgi.py
+++ DBNsite/DBNsite/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DBNsite.settings")
 
 application = get_wsgi_application()
+
+# Added for easy Heroku support:
+from dj_static import Cling
+application = Cling(get_wsgi_application())
