f5a83e10beff701ad2e987d9c3042a6f101b77ec
ggiuffre <giorgiogiuffre@tin.it>
2017-07-25 16:20:07 +0200

    Fix bug on Windows server

--- DBNsite/DBNlogic/train.py
+++ DBNsite/DBNlogic/train.py
@@ -69,8 +69,9 @@ class CDTrainer:
                     data = trainset[start : start + batch_sz].transpose()
                 except ValueError:
                     print('error with npmat')
-                    tmp_data = np.transpose(trainset[start : start + batch_sz].as_numpy_array().tolist()).tolist()
-                    data = gpu.garray(tmp_data)
+                    tmp_data = trainset[start : start + batch_sz].as_numpy_array().tolist()
+                    tmp_data_T = np.transpose(tmp_data).tolist()
+                    data = gpu.garray(tmp_data_T)
 
                 # --> positive phase:
                 pos_hid_probs = (gpu.dot(net.W, data) + net.b.tile(batch_sz)).logistic()
